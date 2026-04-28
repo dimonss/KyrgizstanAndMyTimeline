3e555a478416282324b1513628bdaca8a86f5a59
Dmitrii <faimos69@gmail.com>
2026-04-28 21:38:14 +0600

    Update display logic for language switcher

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { I18nProvider } from './i18n'
+import { I18nProvider } from './I18nProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
